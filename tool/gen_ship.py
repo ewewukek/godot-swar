5cdac5b688ebeb7252920ca3d201eb16586b1009
ewewukek <ewewukek@gmail.com>
2018-06-03 19:02:20 +0300

    textures now store direction vector and 16bit distance

--- tool/gen_ship.py
+++ tool/gen_ship.py
@@ -5,11 +5,11 @@ from PIL import Image, ImageMode
 def sub(p1, p2):
     return (p1[0] - p2[0], p1[1] - p2[1])
 
+def mul(p, k):
+    return (p[0] * k, p[1] * k)
+
 def length(v):
     return sqrt( v[0] ** 2 + v[1] ** 2 )
-
-def dist(p1, p2):
-    return length( sub(p1, p2) )
 
 def normalize(v):
     l = length(v)
@@ -18,19 +18,30 @@ def normalize(v):
 def dot (v1, v2):
     return v1[0] * v2[0] + v1[1] * v2[1];
 
-def dist_seq(p, l1, l2, check_borders = True):
+def vec_dist_seq(p, l1, l2, check_borders = True):
     v = normalize( sub(l2, l1) )
     if check_borders and (dot(v, sub(p, l1) ) < 0 or dot(v, sub(p, l2) ) > 0):
-        return -1
+        return (-1, (0, 0))
 
     e = normalize( (-v[1], v[0]) )
-    return dot(e, (p[0] - l1[0], p[1] - l1[1]) )
+    r = dot(e, (p[0] - l1[0], p[1] - l1[1]) )
+    return (r, e)
 
-def dist2color(r):
-    r_r = round(r)
-    r1 = (r - r_r) * 255
-    r1_r = round(r1)
-    return (min(255, r_r), r1_r, 0)
+def float2int(x, f): # [-1.0, 1.0] => [0, f-1]
+    x = max(-1.0, min(1.0, x))
+    c = (x / 2.0 + 0.5) * (f - 1)
+    if x >= 0.0:
+        return ceil(c)
+    else:
+        return floor(c)
+
+def vec2color(d, v):
+    d_i = float2int(d, 1 << 16)
+
+    x_i = float2int(v[0], 1 << 8)
+    y_i = float2int(v[1], 1 << 8)
+
+    return (x_i, y_i, d_i & 255, 255 - (d_i >> 8))
 
 size = 17.5
 glow_radius = 35
@@ -46,36 +57,47 @@ p2 = (0, -0.5 * size)
 
 ### hull.png
 
-im = Image.new("RGB", (w, h))
+im = Image.new("RGBA", (w, h))
 pix = im.load()
 
 for j in range(0, h):
     for i in range(0, w):
-        x = (0.5 + i - w / 2) / scale
-        if x < 0: x = -x # ship is symmetrical
-        y = (0.5 + h / 2 - j) / scale
+        x = (i - w / 2 + 0.5) / scale
+        y = -(j - h / 2 + 0.5) / scale
+
+        if x < 0:
+            x = -x
+            x_mul = -1
+        else:
+            x_mul = 1
+
         p = (x, y)
 
-        d1 = dist_seq(p, p0, p1)
-        d2 = dist_seq(p, p1, p2)
+        (d1, v1) = vec_dist_seq(p, p0, p1)
+        (d2, v2) = vec_dist_seq(p, p1, p2)
 
         if d1 >= 0:
-            r = d1
+            (d, v) = (d1, v1)
         elif y >= p0[1]:
-            r = dist(p, p0)
+            v = sub(p, p0)
+            (d, v) = (length(v), normalize(v))
         elif d2 >= 0:
-            r = d2
+            (d, v) = (d2, v2)
         elif x >= p1[0] or y <= p1[1]:
-            r = dist(p, p1)
+            v = sub(p, p1)
+            (d, v) = (length(v), normalize(v))
         else:
-            d1 = dist_seq(p, p1, p0, False)
-            d2 = dist_seq(p, p2, p1, False)
-            if d1 < 0 or d2 < 0:
-                raise Exception("something is wrong")
+            (d1, v1) = vec_dist_seq(p, p1, p0, False)
+            (d2, v2) = vec_dist_seq(p, p2, p1, False)
+            assert d1 >= 0.0 and d2 >= 0.0
 
-            r = min(d1, d2)
+            if d1 < d2:
+                (d, v) = (-d1, v1)
+            else:
+                (d, v) = (-d2, v2)
 
-        pix[i, j] = dist2color(r)
+        v = (v[0] * x_mul, v[1])
+        pix[i, j] = vec2color(d / glow_radius, v)
 
 im.show()
 out_path = path.join(path.dirname(path.realpath(__file__)), '..', 'entity', 'ship', 'hull.png')
@@ -83,28 +105,38 @@ im.save(out_path, "PNG")
 
 ### part_front.png
 
-part_size = dist(p0, p1)
+part_size = length(sub(p0, p1))
 w = int(glow_radius * 2 * scale)
 h = int((glow_radius * 2 + part_size) * scale)
 
-im = Image.new("RGB", (w, h))
+im = Image.new("RGBA", (w, h))
 pix = im.load()
 
 p3 = (0, part_size / 2)
 
 for j in range(0, h):
     for i in range(0, w):
-        x = (0.5 + i - w / 2) / scale
-        if x < 0: x = -x
-        y = (0.5 + h / 2 - j) / scale
-        if y < 0: y = -y
-        p = (x, y)
-        if y < p3[1]:
-            r = x
-        else:
-            r = dist(p, p3)
+        x = (i - w / 2 + 0.5) / scale
+        y = -(j - h / 2 + 0.5) / scale
 
-        pix[i, j] = dist2color(r)
+        if y < 0:
+            y = -y
+            y_mul = -1
+        else:
+            y_mul = 1
+
+        p = (x, y)
+
+        if y < p3[1]:
+            v = (x, 0)
+        else:
+            v = sub(p, p3)
+
+        d = length(v)
+        v = normalize(v)
+
+        v = (v[0], v[1] * y_mul)
+        pix[i, j] = vec2color(d / glow_radius, v)
 
 im.show()
 out_path = path.join(path.dirname(path.realpath(__file__)), '..', 'entity', 'ship', 'part_front.png')
@@ -112,28 +144,38 @@ im.save(out_path, "PNG")
 
 ### part_back.png
 
-part_size = dist(p1, p2)
+part_size = length(sub(p1, p2))
 w = int(glow_radius * 2 * scale)
 h = int((glow_radius * 2 + part_size) * scale)
 
-im = Image.new("RGB", (w, h))
+im = Image.new("RGBA", (w, h))
 pix = im.load()
 
 p4 = (0, part_size / 2)
 
 for j in range(0, h):
     for i in range(0, w):
-        x = (0.5 + i - w / 2) / scale
-        if x < 0: x = -x
-        y = (0.5 + h / 2 - j) / scale
-        if y < 0: y = -y
-        p = (x, y)
-        if y < p4[1]:
-            r = x
-        else:
-            r = dist(p, p4)
+        x = (i - w / 2 + 0.5) / scale
+        y = -(j - h / 2 + 0.5) / scale
 
-        pix[i, j] = dist2color(r)
+        if y < 0:
+            y = -y
+            y_mul = -1
+        else:
+            y_mul = 1
+
+        p = (x, y)
+
+        if y < p4[1]:
+            v = (x, 0)
+        else:
+            v = sub(p, p4)
+
+        d = length(v)
+        v = normalize(v)
+
+        v = (v[0], v[1] * y_mul)
+        pix[i, j] = vec2color(d / glow_radius, v)
 
 im.show()
 out_path = path.join(path.dirname(path.realpath(__file__)), '..', 'entity', 'ship', 'part_back.png')
@@ -142,23 +184,24 @@ im.save(out_path, "PNG")
 
 ### particle.png
 
-w = int(20 * scale)
-h = int(20 * scale)
+glow_radius = 10
 
-im = Image.new("RGB", (w, h))
+w = int(glow_radius * 2 * scale)
+h = int(glow_radius * 2 * scale)
+
+im = Image.new("RGBA", (w, h))
 pix = im.load()
-
-p5 = (0, 0)
 
 for j in range(0, h):
     for i in range(0, w):
-        x = (0.5 + i - w / 2) / scale
-        if x < 0: x = -x
-        y = (0.5 + h / 2 - j) / scale
-        if y < 0: y = -y
-        p = (x, y)
+        x = (i - w / 2 + 0.5) / scale
+        y = -(j - h / 2 + 0.5) / scale
 
-        pix[i, j] = dist2color( dist(p, p5) )
+        v = (x, y)
+        d = length(v)
+        v = normalize(v)
+
+        pix[i, j] = vec2color(d / glow_radius, v)
 
 im.show()
 out_path = path.join(path.dirname(path.realpath(__file__)), '..', 'entity', 'particle.png')
